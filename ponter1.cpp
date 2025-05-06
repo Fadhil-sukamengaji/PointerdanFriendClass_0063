82f243576eada6f39c1e0c7161db1706cb945c1f
FadhilM <mudzakifadhil@gmail.com>
2025-05-06 15:19:27 +0700

    input object (P1)

--- ponter1.cpp
+++ ponter1.cpp
@@ -9,4 +9,16 @@ class mahasiswa
     {
         cout << "No Induk = " << nim << endl;
     }
+};
+
+int main()
+{
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); //member acces operator
+
+    mahasiswa &refMhs = mhs; //pointer reference refmhs
+    refMhs.nim = 2; //member acces operator
+    mhs.showNim();
+
+    
 }
