c4e3859606f5d56b48654ea9f97f44fe363c7fe7
FadhilM <mudzakifadhil@gmail.com>
2025-05-06 14:58:06 +0700

    menambahkan class Lingkaran

--- P1.cpp
+++ P1.cpp
@@ -15,4 +15,29 @@ class PersegiPanjang
             cout << "masukkan lebar : ";
             cin >> lebar;
         }
-}
+
+        int luasPp(int a, int b)
+        {
+            return a * b;
+        }
+
+        Lingkaran ol;
+
+};
+
+class Lingkaran
+{
+    public:
+        int jarijari;
+
+        void input()
+        {
+            cout << "masukkan jari-jari : ";
+            cin >> jarijari;
+        }
+
+        int luasLingkaran(int r)
+        {
+            return 3.14 * r * r;
+        }
+};
